3ea7aa64a11b2135bca0043653c8ffafce41b42a
Luco06 <luc-olivier.yohan@hotmail.fr>
2021-10-13 16:49:39 +0200

    factorisarion des images

--- src/Components/skill/skill.jsx
+++ src/Components/skill/skill.jsx
@@ -1,67 +1,114 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './skill.scss';
-import angular from '../../assets/images/angular.png';
-import boots from '../../assets/images/boots.png';
-import css from '../../assets/images/css.png';
-import html from '../../assets/images/html.png';
-import java from '../../assets/images/java.png';
-import js from '../../assets/images/js.png';
-import mysql from '../../assets/images/mysql.png';
-import php from '../../assets/images/php.png';
-import ps from '../../assets/images/ps.png';
-import py from '../../assets/images/py.png';
-import react from '../../assets/images/react.png';
-import sass from '../../assets/images/sass.png';
-import vue from '../../assets/images/vue.png';
-import node from '../../assets/images/Nodejs.png';
 
+const LogoSkill1 = [
+    {
+        id: 1,
+        image: "https://i.ibb.co/8212xYC/angular.png"
+    },
+    {
+        id: 2,
+        image: "https://i.ibb.co/r0DMCXv/css.png"
+    },
+    {
+        id: 3,
+        image: "https://i.ibb.co/wRhXxyB/html.png"
+    },
+    {
+        id: 4,
+        image: "https://i.ibb.co/tBV1C9K/java.png"
 
+    }
+]
+const LogoSkill2 = [
 
-function Skill () {
+    {
+        id: 5,
+        image: "https://i.ibb.co/BKv3K9G/js.png"
+
+    },
+    {
+        id: 6,
+        image: "https://i.ibb.co/cwgyj5z/mysql.png"
+
+    },
+    {
+        id: 7,
+        image: "https://i.ibb.co/VjC6g7G/Nodejs.png"
+
+    },
+    {
+        id: 8,
+        image: "https://i.ibb.co/k8pP6cw/php.png"
+
+    }
+]
+const LogoSkill3 = [
+    {
+        id: 9,
+        image: "https://i.ibb.co/KFM9Xdv/ps.png"
+
+    },
+    {
+        id: 10,
+        image: "https://i.ibb.co/NpH8r4W/py.png"
+
+    },
+    {
+        id: 11,
+        image: "https://i.ibb.co/Sx1PHyp/react.png"
+
+    },
+
+]
+const LogoSkill4 = [
+
+    {
+        id: 12,
+        image: "https://i.ibb.co/yPjPXTc/sass.png"
+
+    },
+    {
+        id: 13,
+        image: "https://i.ibb.co/K53ccBJ/vue.png"
+
+    }
+]
+function Skill() {
     const [title, setTitle] = useState('Skill')
-    
-        return (
-            <React.Fragment>
-                <div className="skill_title" id="skill">
-                    <div className="barre_gauche"></div>
-                    <button className="btn_skill"><h2 className='skill_h2'>{title}</h2></button>
-                    <div className="barre_droite"></div>
+
+    return (
+        <React.Fragment>
+            <div className="skill_title" id="skill">
+                <div className="barre_gauche"></div>
+                <button className="btn_skill"><h2 className='skill_h2'>{title}</h2></button>
+                <div className="barre_droite"></div>
+            </div>
+            <div className="skill_items" >
+
+                <div className="item1">
+                    {LogoSkill1.map(({ id, image }) => (
+                        <img className="img_item1" key={id} src={image} alt="skillLogo" />
+                    ))}
                 </div>
-                    <div className="skill_items" >
-
-                        <div  className="item1">
-                        <img className="img_item1" src={angular} alt="Angular"/>
-                        <img className="img_item1" src={boots} alt="Bootstrap"/>
-                        <img className="img_item1" src={css} alt="Css"/>
-                        </div>
-
-                        <div   className="item2">
-                        <img className="img_item2"  src={vue} alt="Vue.js"/>
-                        <img className="img_item2"  src={react} alt="React"/>
-                        <img className="img_item2"  src={mysql} alt="Mysql"/>
-                        </div>
-
-                        <div  className="item3">
-                        <img className="img_item3" src={php} alt="PHP"/>
-                        <img className="img_item3" src={py} alt="Python"/>
-                        <img className="img_item3" src={html} alt="HTML"/>
-                        </div>
-
-                        <div  className="item4">
-                        <img className="img_item4" src={sass} alt="SASS"/>
-                        <img className="img_item4" src={java} alt="Java"/>
-                        <img className="img_item4" src={node} alt="Node.Js"/>
-                        </div>
-
-                        <div  className="item5">
-                        <img className="img_item5" src={js} alt="Javascript"/>
-                        <img className="img_item5" src={ps} alt="PhotoShop"/>
-                        </div>
-
-
-                    </div>
-            </React.Fragment>
-        );
+                <div className="item2">
+                    {LogoSkill2.map(({ id, image }) => (
+                        <img className="img_item2" key={id} src={image} alt="skillLogo" />
+                    ))}
+                </div>
+                <div className="item3">
+                    {LogoSkill3.map(({ id, image }) => (
+                        <img className="img_item3" key={id} src={image} alt="skillLogo" />
+                    ))}
+                </div>
+                <div className="item4">
+                    {LogoSkill4.map(({ id, image }) => (
+                        <img className="img_item4" key={id} src={image} alt="skillLogo" />
+                    ))}
+                </div>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default Skill;
